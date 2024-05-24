7530b17bd9af789eac48a3e2801e494161c20888
Siyarbekir <ramazan.du97@gmail.com>
2024-05-24 12:10:22 +0200

    Update main.dart to use Home widget as the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("MyCoffeeShop"),
-        backgroundColor: Colors.brown[700],
-        centerTitle: true,
-      ),
-      body: const Text("Hello World!!"),
+  runApp(
+    const MaterialApp(
+      home: Home(),
     ),
-  ));
+  );
 }
